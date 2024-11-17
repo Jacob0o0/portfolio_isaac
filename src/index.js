007a5cfb1f4fe741506059d9b648811e2fa1d8f7
Jacob0o0 <jacoboe127@gmail.com>
2024-11-16 23:01:46 -0600

    Issue with GH pages

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import Projects from './Projects.tsx';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode basename="portfolio_isaac/">
+  <React.StrictMode>
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
